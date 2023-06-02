484b90e0cb3f10a8c19a1df92820496520e4c947
harshitsaini221 <harshitsaini221git config --global user.name harshitsaini221>
2023-06-02 11:10:09 +0530

    App.js Prettier

--- App.js
+++ App.js
@@ -1,37 +1,40 @@
-import { useEffect } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Button, View } from 'react-native';
-import * as Notifications from 'expo-notifications';
+import { useEffect } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Button, View } from "react-native";
+import * as Notifications from "expo-notifications";
 
 Notifications.setNotificationHandler({
   handleNotification: async () => {
     return {
       shouldPlaySound: false,
       shouldSetBadge: false,
-      shouldShowAlert: true
+      shouldShowAlert: true,
     };
-  }
+  },
 });
 
 export default function App() {
-
   //using useEffect for adding Event Listeners,
   //it is also recommended to use event handler remover to handle memory leaks
 
   useEffect(() => {
-    const subscription1 = Notifications.addNotificationReceivedListener((notification) => {
-      console.log('NOTIFICATION RECEIVED');
-      console.log(notification);
-      const userName = notification.request.content.data.userName;
-      console.log(userName);
-    });
+    const subscription1 = Notifications.addNotificationReceivedListener(
+      (notification) => {
+        console.log("NOTIFICATION RECEIVED");
+        console.log(notification);
+        const userName = notification.request.content.data.userName;
+        console.log(userName);
+      }
+    );
 
-    const subscription2 = Notifications.addNotificationResponseReceivedListener((response) => {
-      console.log('NOTIFICATION RESPONSE RECEIVED');
-      console.log(response);
-      const userName = response.notification.request.content.data.userName;
-      console.log(userName);
-    });
+    const subscription2 = Notifications.addNotificationResponseReceivedListener(
+      (response) => {
+        console.log("NOTIFICATION RESPONSE RECEIVED");
+        console.log(response);
+        const userName = response.notification.request.content.data.userName;
+        console.log(userName);
+      }
+    );
 
     return () => {
       subscription1.remove();
@@ -42,13 +45,13 @@ export default function App() {
   function scheduleNotificationHandler() {
     Notifications.scheduleNotificationAsync({
       content: {
-        title: 'My first local notification',
-        body: 'This is the body of the notification.',
-        data: { userName: 'Harshit' }
+        title: "My first local notification",
+        body: "This is the body of the notification.",
+        data: { userName: "Harshit" },
       },
       trigger: {
-        seconds: 5
-      }
+        seconds: 5,
+      },
     });
   }
 
@@ -66,8 +69,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
