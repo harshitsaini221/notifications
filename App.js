b23ab18dbde43b2eb9c6c0d48f019884b984f934
harshitsaini221 <harshitsaini221git config --global user.name harshitsaini221>
2023-06-02 11:08:49 +0530

    Additions for Removing the App From Drawer

--- App.js
+++ App.js
@@ -1,81 +1,73 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Button, View } from "react-native";
-import * as Notifications from "expo-notifications";
+import { useEffect } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Button, View } from 'react-native';
+import * as Notifications from 'expo-notifications';
 
 Notifications.setNotificationHandler({
   handleNotification: async () => {
     return {
       shouldPlaySound: false,
       shouldSetBadge: false,
-      shouldShowAlert: true,
+      shouldShowAlert: true
     };
-  },
+  }
 });
- 
- 
-//// START: NEWLY ADDED FUNCTIONS ////
-const allowsNotificationsAsync = async () => {
-  const settings = await Notifications.getPermissionsAsync();
-  return (
-    settings.granted ||
-    settings.ios?.status === Notifications.IosAuthorizationStatus.PROVISIONAL
-  );
-};
- 
-const requestPermissionsAsync = async () => {
-  return await Notifications.requestPermissionsAsync({
-    ios: {
-      allowAlert: true,
-      allowBadge: true,
-      allowSound: true,
-      allowAnnouncements: true,
-    },
-  });
-};
-//// END: NEWLY ADDED FUNCTIONS ////
- 
+
 export default function App() {
-  const scheduleNotificationHandler = async () => {
- 
-    //// START: CALL FUNCTIONS HERE ////
-    const hasPushNotificationPermissionGranted =
-      await allowsNotificationsAsync();
- 
- 
-    if (!hasPushNotificationPermissionGranted) {
-      await requestPermissionsAsync();
-    }
-    //// END: CALL FUNCTIONS HERE ////
- 
+
+  //using useEffect for adding Event Listeners,
+  //it is also recommended to use event handler remover to handle memory leaks
+
+  useEffect(() => {
+    const subscription1 = Notifications.addNotificationReceivedListener((notification) => {
+      console.log('NOTIFICATION RECEIVED');
+      console.log(notification);
+      const userName = notification.request.content.data.userName;
+      console.log(userName);
+    });
+
+    const subscription2 = Notifications.addNotificationResponseReceivedListener((response) => {
+      console.log('NOTIFICATION RESPONSE RECEIVED');
+      console.log(response);
+      const userName = response.notification.request.content.data.userName;
+      console.log(userName);
+    });
+
+    return () => {
+      subscription1.remove();
+      subscription2.remove();
+    };
+  }, []);
+
+  function scheduleNotificationHandler() {
     Notifications.scheduleNotificationAsync({
       content: {
-        title: "My first local notification",
-        body: "This is th body of the notification.",
-        data: { userName: "100packabs" },
+        title: 'My first local notification',
+        body: 'This is the body of the notification.',
+        data: { userName: 'Harshit' }
       },
       trigger: {
-        seconds: 2,
-      },
+        seconds: 5
+      }
     });
-  };
- 
+  }
+
   return (
     <View style={styles.container}>
       <Button
-        title='Schedule Notification'
+        title="Schedule Notification"
         onPress={scheduleNotificationHandler}
       />
- 
-      <StatusBar style='auto' />
+      <StatusBar style="auto" />
     </View>
   );
 }
- 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
